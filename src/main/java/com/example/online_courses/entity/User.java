acfa934373ee196fd0edf0931b6cb207338a9925
Qing Deng <qd319@nyu.edu>
2021-12-10 08:30:05 -0500

    added two new fields

--- src/main/java/com/example/online_courses/entity/User.java
+++ src/main/java/com/example/online_courses/entity/User.java
@@ -1,11 +1,18 @@
 package com.example.online_courses.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class User {
+    @TableId(type=IdType.ASSIGN_ID)
     private Long id;
     private String name;
     private Integer age;
     private String email;
+    private Date createTime;
+    private Date editTime;
 }
--- src/test/java/com/example/online_courses/OnlineCoursesApplicationTests.java
+++ src/test/java/com/example/online_courses/OnlineCoursesApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.online_courses;
 
 import com.example.online_courses.entity.User;
 import com.example.online_courses.mapper.UserMapper;
+import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,5 +21,24 @@ class OnlineCoursesApplicationTests {
         List<User> userList =  userMapper.selectList(null);
         System.out.println(userList);
     }
+
+    @Test
+    void addUser(){
+        User user = new User();
+        user.setName("Lucy");
+        user.setAge(53);
+        user.setEmail("lucy@youtube.com");
+        int insert = userMapper.insert(user);
+        System.out.println("insert result:" + insert);
+    }
+
+    @Test
+    void updateUser(){
+        User user = new User();
+        user.setId(2L);
+        String name = user.getName();
+        System.out.println(user);
+    }
+
 
 }
